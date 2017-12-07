44e8c2bdb6181476af308f66397cfc3b715fee2a
Micooz <micooz@hotmail.com>
2017-12-07 09:32:43 +0800

    Add sass module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from './index.scss';
 
-const App = () => <div>Hello Parcel x React</div>;
+const App = () => <div className={styles.app}>Hello Parcel x React</div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
